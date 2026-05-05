76e3eb6941ba5b4676985571f84d9d15dfd99815
eltzhanna <eltz.hanna@gmail.com>
2026-05-04 21:44:15 -0300

    altera repositorio

---
+++ aula03/atividade01.py
@@ -0,0 +1,23 @@
+valor = float(input('informe o valor:'))
+if valor > 250:
+    desconto = valor * 0.16
+    valor_final  = valor - desconto
+    print(f' o valor com desconto de 16% R${valor_final}')
+else:
+    print(f'valor sem desconto: R$ {valor}')
+    
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
